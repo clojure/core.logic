03b0e4044a0bba34bf9a6628bc07e9d5cf7b99ee
David Nolen <david.nolen@gmail.com>
2010-11-25 10:37:20 -0500

    * README: initial commit

---
+++ src/logic_fun/core.clj
@@ -0,0 +1,14 @@
+(ns logic-fun.core
+  (:require [clojure.core.unify :as unify]))
+
+;; pure miniKanren
+
+
+
+;; changes
+
+
+
+
+(comment
+  )
---
+++ test/kanren_fun/test/core.clj
@@ -0,0 +1,6 @@
+(ns kanren-fun.test.core
+  (:use [kanren-fun.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
