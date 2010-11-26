0bc0daf77c9e59c9a2571fd47ef305ddcf528b75
David Nolen <david.nolen@gmail.com>
2010-11-26 13:24:06 -0500

    * project.clj: woot, we're much faster than the Scheme version for the degenerate lookup cases

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject logic-fun "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/core.unify "0.5.0-SNAPSHOT"]
                  [net.intensivesystems/mini-kanren "1.0.0-dev-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
--- src/logic_fun/fast.clj
+++ src/logic_fun/fast.clj
@@ -1,133 +1,210 @@
 (ns logic-fun.fast
   (:use [clojure.pprint :only [pprint]]))
 
-;; Notes on how substitution works
-;; 1) search until we find a value
-;; 2) disallow addition substitutions that go back to the var we're looking for
+(set! *warn-on-reflection* true)
+
+;; =============================================================================
+;; Utilities
 
 (defn vec-last [v]
   (nth v (dec (count v))))
 
-(defrecord lvarT [name])
+;; =============================================================================
+;; Logic Variables
+
+(deftype lvarT [name])
 
 (defn lvar? [x]
   (instance? lvarT x))
 
-(defn lvar [name] (lvarT. name))
+(defn ^lvarT lvar
+  ([] (lvarT. (gensym)))
+  ([name] (lvarT. name)))
 
 (defmethod print-method lvarT [x writer]
-           (.write writer (str "<lvar:" (:name x) ">")))
+           (.write writer (str "<lvar:" (.name ^lvarT x) ">")))
 
-;; NOTE: consider lowering to deftype
-(defrecord pairT [lhs rhs])
+;; TODO : why doesn't print-method get called during pretty printing ?
 
-(defn pair [lhs rhs]
+;; =============================================================================
+;; Pairs
+
+(defprotocol IPair
+  (lhs [this])
+  (rhs [this]))
+
+(defrecord pairT [lhs rhs]
+  IPair
+  (lhs [this] lhs)
+  (rhs [this] rhs)
+  ;; TODO : support destructuring
+  ;; clojure.lang.Indexed
+  ;; (nth [this idx] (case idx 0 lhs 1 rhs))
+  ;; (nth [this idx default] (case idx 0 lhs 1 rhs :else default))
+  )
+
+(defn ^pairT pair [lhs rhs]
   (pairT. lhs rhs))
+
+;; =============================================================================
+;; Substitutions
 
 (defprotocol ISubstitutions
   (length [this])
   (ext [this x v])
-  (lookup [this v]))
+  (ext-no-check [this x v])
+  (lookup [this v])
+  (unify [this u v]))
 
-(defn lookup* [ss v]
-  (loop [v v a (vec-last (ss v)) ss ss orig v]
+(defn lookup* [s v]
+  (loop [v v v' (vec-last (s v)) s s ov v]
     (cond
-     (nil? a)  v
-     (= a orig) :circular
-     (lvar? a) (recur a (vec-last (ss a)) ss orig)
-     :else a)))
+     (nil? v')          v
+     (identical? v' ov) :circular
+     (lvar? v')         (recur v' (vec-last (s v')) s ov)
+     :else              v')))
 
-(defrecord Substitutions [ss order]
+(declare unify*)
+
+(defrecord Substitutions [s order]
   ISubstitutions
+  (length [this] (count order))
   (ext [this x v]
-       (if (= (lookup* ss x) :circular)
+       (if (= (lookup* s x) :circular)
          nil
-         (Substitutions. (update-in ss [x] (fnil conj []) v)
-                         (conj order x))))
+         (ext-no-check this x v)))
+  (ext-no-check [this x v]
+                (Substitutions. (update-in s [x] (fnil conj []) v)
+                                (conj order x)))
   (lookup [this v]
-          (lookup* ss v)))
+          (lookup* s v))
+  (unify [this u v]
+         (unify* s u v)))
 
-(defn empty-s []
+(defn ^Substitutions empty-s []
   (Substitutions. {} []))
 
+;; =============================================================================
+;; Unification
+
+;; in Fogus's core.unify, the unifier deals w/ construction
+;; miniKanren only deals with substitutions
+
+(defn unify* [s u v]
+  (let [u (lookup u s)
+        v (lookup v s)]
+    (cond
+     (identical? u v) s
+     (lvar? u) (if (lvar? v)
+                 (ext-no-check s u v)
+                 (ext s u v)))))
+
+;; =============================================================================
+;; Comments and Testing
+
 (comment
-  ;; remember backwards for us since we're using vectors
-  [[x 1] [y 5] [x y]]
+ ;; ==================================================
+ ;; TESTS
 
   ;; prevent circular substs
   (let [x  (lvar 'x)
         y  (lvar 'y)
-        ss (Substitutions. {x [y] y [x]} [x y])]
-    (ext ss y y))
+        s (Substitutions. {x [y] y [x]} [x y])]
+    (ext s y y))
   
- ;; test
+  
  (let [x  (lvar 'x)
        y  (lvar 'y)
        z  (lvar 'z)
        ss (Substitutions. {x [1 y]
                            y [5]}
-                          [x y x])
-       ss (ext ss x z)]
-   (println ss))
+                          [x y x])]
+   (ext ss x z))
+ 
 
- ;; not bad 500ms
+ (let [x  (lvar 'x)
+       y  (lvar 'y)
+       ss (Substitutions. {x [1 y]
+                           y [5]}
+                          [x y x])]
+   (lookup ss x))
+
+ ;; ==================================================
+ ;; PERFORMANCE
+ 
+ ;; sick 470ms on 1.3.0 alph3
  (dotimes [_ 10]
-   (let [[x y z] [(lvar) (lvar) (lvar)]
-         ss (Substitutions. {x [y 1] y [5]} [x y x])]
+   (let [[x y z :as s] (map lvar '[x y z])
+         ss (Substitutions. {x [y 1] y [5]} s)]
+     (time
+      (dotimes [_ 1e6]
+        (ext-no-check ss x z)))))
+
+ ;; ~650ms
+ (dotimes [_ 10]
+   (let [[x y z :as s] (map lvar '[x y z])
+         ss (Substitutions. {x [y 1] y [5]} s)]
      (time
       (dotimes [_ 1e6]
         (ext ss x z)))))
 
- ;; ~300ms, pretty good, neet to compare to naive
- ;; for this degenerate case ~500ms
+ ;; ~1200ms
+ ;; just a tiny bit slower than the Scheme version
  (dotimes [_ 10]
-   (let [[x y z] [(lvar) (lvar) (lvar)]
-         ss (Substitutions. {x [y] y [z] z [5]} [x y z])]
-     (time
-      (dotimes [_ 1e6]
-        (lookup ss x)))))
-
- ;; ~900ms
- ;; just a tiny bit faster than scheme
- (dotimes [_ 10]
-   (let [[x y z c b a] [(lvar) (lvar) (lvar) (lvar) (lvar) (lvar)]
-         ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]} [x y z c b a])]
+   (let [[x y z c b a :as s] (map lvar '[x y z c b a])
+         ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]} s)]
      (time
       (dotimes [_ 1e6]
         (lookup ss a)))))
 
  ;; degenerate case
- ;; same speed as above
- ;; Scheme is ~1130ms
- ;; so Scheme is doing okay, assq must be pretty quick
+ (let [[x m y n z o c p b q a] (map lvar '[x m y n z o c p b q a])
+       ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]
+                           m [0] n [1] o [2] p [3] q [4]}
+                          [x m m m m y n n n n z o o o o
+                           c p p p p b q q q q a])]
+   (lookup ss a))
+
+ ;; 600ms (NOTE: this jump is because array-map is slower than hash-maps)
+ ;; Scheme is ~1650ms
  (dotimes [_ 10]
-   (let [[x m y n z o c p b q a] (take 11 (repeatedly lvar))
-         ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]} [x m y n z o c p b q a])]
+   (let [[x m y n z o c p b q a] (map lvar '[x m y n z o c p b q a])
+         ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]
+                             m [0] n [1] o [2] p [3] q [4]}
+                            [x m m m m y n n n n z o o o o
+                             c p p p p b q q q q a])]
      (time
       (dotimes [_ 1e6]
         (lookup ss a)))))
-
- (let [[x m y n z o c p b q a] (take 11 (repeatedly lvar))
-         ss (Substitutions. {x [5] y [x] z [y] c [z] b [c] a [b]} [x m y n z o c p b q a])]
-     (time
-      (lookup ss a)))
-
- ;; erg before we get ahead of ourselves
- ;; preventing circularity
-)
+ )
 
 (comment
+  (dotimes [_ 10]
+    (let [v [1 2 3 4 5 6 7 8 9 0]]
+     (time
+      (dotimes [_ 1e6]
+        (vec-last v)))))
 
-  ;; damn doesn't work
-  (let [x []]
-    (case (class x)
-          clojure.lang.PersistentVector 'a
-          clojure.lang.PersistentList 'b
-          clojure.lang.PersistenHashMap 'c))
+  ;; interesting, destructuring is slow because we calling clojure.core/nth
+  ;; instead of our own
+  (let [p (pair 1 2)]
+    (dotimes [_ 10]
+      (time
+       (dotimes [_ 1e7]
+         (let [[a b] p])))))
 
-  (let [x 0]
-    (case x
-          0 'a
-          1 'b
-          2 'c))
+  ;; getting distract should look into what's going on here later
+  (let* [p (pair 1 2)]
+        (dotimes [_ 10]
+          (time
+           (dotimes [_ 1.0E7]
+             (let* [vec__4455 p
+                    a (nth vec__4455 0 nil)
+                    b (nth vec__4455 1 nil)])))))
+
+  (let [p [1 2]]
+    (dotimes [_ 10]
+      (time
+       (dotimes [_ 1e7]
+         (let [[a b] p])))))
 )
