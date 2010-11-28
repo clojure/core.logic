59d1d447724364435025ff30b9444c514e795cb4
David Nolen <david.nolen@gmail.com>
2010-11-28 18:17:59 -0500

    * project.clj: cleanup

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject logos "0.1.0-SNAPSHOT"
   :description "Logic programming for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0-alpha3"]
-                 #_[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/core.unify "0.5.0-SNAPSHOT"]
-                 [net.intensivesystems/mini-kanren "1.0.0-dev-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
