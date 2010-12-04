0fe461f221e2fab8f9d0fb235388a502636e990f
David Nolen <david.nolen@gmail.com>
2010-12-04 12:29:38 -0500

    * src/logos/examples.clj: examples/notes

--- src/logos/examples.clj
+++ src/logos/examples.clj
@@ -2,10 +2,11 @@
   (:refer-clojure :exclude [reify inc == take])
   (:use logos.minikanren))
 
-(defn likes [x y]
+(defn likes
+  [x y]
   (cond-e
    ((== x 'john) (== y 'mary))
-   ((== x 'mary) (== y 'join))))
+   ((== x 'mary) (== y 'john))))
 
 (defn musician [x]
   (cond-e
@@ -16,20 +17,55 @@
 
   (run* [q]
         (likes q 'mary)
-        (musician q)) ;; [john]
+        (musician q))
+  
+  ;; [john]
 
   (run* [q]
-        (musician q)) ;; [john bob]
+        (musician q))
+
+  (run* [q]
+        (exist [x y]
+               (likes x y)
+               (== (cons x (cons y '())) q)))
+  
+  ;; [john bob]
+
+  ;; this is already useful of course
 
   ;; not the question is how would one go about creating
-  ;; a knowledge base of facts and chaining them together?
-
-  ;; that is it important to keep the system open?
+  ;; a knowledge base of facts and chaining them together
+  ;; in an open manner?
 
   ;; (?- likes 'john 'mary)
+  ;; first time, creates a function likes of two arities
+  ;; 2 argument, and 3 argument one that takes a next
+  ;; parameter
+  
   ;; (?- likes 'mary 'john)
   ;; (?- likes ?x 'mary)
- )
+
+  ;; chained goals?
+  )
+
+(defn likes
+  ([x y]
+     (likes x y u#))
+  ([x y next]
+     (cond-e
+      ((== x 'john) (== y 'mary))
+      ((== x 'mary) (== y 'join))
+      (next))))
+
+
+(defn g1 [x g2]
+  (cond-e
+   ((== x 'foo))
+   (g2)))
+
+;; interesting we can pass goals as parameters
+(run* [q]
+      (g1 q (== q 'foo)))
 
 (comment
   (defmulti multi-test type)
