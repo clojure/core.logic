1735b8d96a502fb037249440bd63054689d67f1c
David Nolen <david.nolen@gmail.com>
2010-12-22 00:58:54 -0500

    * src/logos/scratch.clj: some thoughts about juxt's striking relationship's to cond-e

--- src/logos/scratch.clj
+++ src/logos/scratch.clj
@@ -1,12 +1,19 @@
 (ns logos.scratch
   (:refer-clojure :exclude [reify ==]))
 
-(defn sum [acc n]
-  (if (zero? n)
-    acc
-    (recur (inc acc) (dec n))))
-
-(sum 0 1000000)
+;; juxt is like cond-e
+;; mapcat can combine the result of juxt
+;; and we have interleave (it just doesn't keep going)
+;; suggest weave
 
 (comment
+
+  (->> '[a0 a1]
+       (mapcat (juxt identity #(symbol (str % "'")))))
+
+  ;; we could jus forgo the whole goals thing altogether
+  ;; just compile functions that work in the right manner
+
+  ;; can we do bind with some lazy combination of Clojure fns?
+
   )
