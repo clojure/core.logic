01147d49b04d26c395d5b3e381110b49f8f6b3c9
David Nolen <david.nolen@gmail.com>
2011-01-23 22:23:01 -0500

    * src/logos/match.clj: sketch

--- src/logos/match.clj
+++ src/logos/match.clj
@@ -3,6 +3,13 @@
   (:use logos.minikanren)
   (:import [logos.minikanren Substitutions]))
 
+(defmacro defn-e [args c*]
+  ~(fn [~@args] (handle-clauses [~@args] ~@c*)))
+
+(defmacro match-e [e c & c*]
+  `(let [t e]
+     (handle-clauses t ~@c*)))
+
 (comment
   (defn-e append [x y z]
     ([() _ y])
