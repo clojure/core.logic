d6f86b82d943e43da1b2e11e9a310431fe9547b5
David Nolen <david.nolen@gmail.com>
2011-03-14 17:44:27 -0400

    * src/logos/match.clj: we have something works, very ugly

--- src/logos/match.clj
+++ src/logos/match.clj
@@ -1,7 +1,8 @@
 (ns logos.match
   (:refer-clojure :exclude [reify == inc])
   (:use logos.minikanren)
-  (:require [logos.logic :as logic])
+  (:require [logos.logic :as logic]
+            [clojure.set :as set])
   (:import [logos.minikanren Substitutions]))
 
 (defmacro defn-e [n as & cs]
@@ -26,36 +27,40 @@
 (defn lvar-sym? [s]
   (= (first (str s)) \?))
 
-;; TODO: cleanup
-
 (defn exist-expr? [[f & r]]
   (= f `exist))
 
-(defn unify-p [[fp & rp :as p] [fa & ra :as a] exprs]
+;; TODO: redesign
+
+(defn unify-p [[fp & rp :as p] [fa & ra :as a] seen exprs]
   (if (not (nil? (seq p)))
-    (let [ex (cond
-              (= fp '_) nil
-              (and (coll? fp)
-                   (seq fp)) `(exist [~@(extract-vars fp)]
-                                                (== ~(unifier-term fp) ~fa))
-              (lvar-sym? fp) `(exist [~fp]
-                                     (== ~fp ~fa))
-              :else          `(exist []
-                                     (== ~fp ~fa)))
-          r (unify-p rp ra exprs)]
+    (let [[ex new-vars] (cond
+                         (= fp '_) nil
+                         (and (coll? fp)
+                              (seq fp)) (let [vs (set/difference (extract-vars fp) seen)]
+                                          [`(exist [~@vs]
+                                                   (== ~(unifier-term fp) ~fa)) vs])
+                              (and (lvar-sym? fp)
+                                   (not (contains? seen fp)))
+                                         [`(exist [~fp]
+                                                 (== ~fp ~fa))
+                                          [fp]]
+                              :else      [`(exist []
+                                                  (== ~fp ~fa)) []])
+          r (unify-p rp ra (reduce conj seen new-vars) exprs)]
       (if r
         (if ex
           (let [r (if (exist-expr? r)
                     (list r)
                     r)]
-           (concat ex r))
+            (concat ex r))
           r)
         ex))
     exprs))
 
 (defn handle-clause [a*]
   (fn [[p & ex :as c]]
-    (unify-p p a* ex)))
+    (unify-p p a* #{} ex)))
 
 (defn handle-clauses [as cs]
   `(cond-e
