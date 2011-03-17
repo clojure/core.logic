45c56044a91707c8093eadd283a7db7fe2ec730b
David Nolen <david.nolen@gmail.com>
2011-03-17 01:13:11 -0400

    * project.clj: update version, remove files that are just sitting around taking up space

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject logos "0.5.1-SNAPSHOT"
+(defproject logos "0.5.2-SNAPSHOT"
   :description "Logic programming for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0-alpha5"]
                  [org.clojure.contrib/macro-utils "1.3.0-alpha4"]
