0a0b2527ff57dd9f9c05af766990c694b64d99da
David Nolen <david.nolen@gmail.com>
2011-03-22 15:34:43 -0400

    * src/logos/disequality.clj: prefix notes

--- src/logos/disequality.clj
+++ src/logos/disequality.clj
@@ -29,7 +29,7 @@
       nil)
     true))
 
-(defn merge-contraints [c1 c2]
+(defn merge-constraints [c1 c2]
   )
 
 (defn prefix [s <s]
@@ -46,11 +46,13 @@
                (not sp) this
                (= this sp) nil
                :else (let [[[k v] & r :as c] (into {} (prefix (.l sp) (.l this)))
-                           meta (if (= (count c) 1)
-                                  {:simple #{v} :complex []}
-                                  {:simple #{} :complex [c]})
+                           nc (if (= (count c) 1)
+                                {:simple #{v} :complex []}
+                                {:simple #{} :complex [c]})
                            ks (keys c)
-                           nks (zipmap ks (map #(with-meta % meta) ks))
+                           nks (zipmap ks (map #(with-meta %
+                                                  (merge-constraints (meta %) nc))
+                                               ks))
                            os (.s this)]
                        (Substitutions. (rename-keys os nks)
                                        (.l this) constraint))))))
@@ -64,4 +66,18 @@
                 (ext-no-check y 2))
         s2 (ext-no-check s1 z 3)]
     (prefix (.l s2) (.l s1)))
+
+  ;; plenty fast
+  ;; 1s
+  (dotimes [_ 10]
+    (time
+     (dotimes [_ 2e6]
+       (let [x (lvar 'x)
+             y (lvar 'y)
+             z (lvar 'z)
+             s1 (-> empty-s
+                    (ext-no-check x 1)
+                    (ext-no-check y 2))
+             s2 (ext-no-check s1 z 3)]
+         (prefix (.l ^Substitutions s2) (.l ^Substitutions s1))))))
   )
