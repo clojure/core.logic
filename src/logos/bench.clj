883a103751afb85031870a3328382cc4c6ac681a
David Nolen <david.nolen@gmail.com>
2011-04-03 22:17:18 -0400

    * src/logos/bench.clj: tweaks

--- src/logos/bench.clj
+++ src/logos/bench.clj
@@ -92,44 +92,48 @@
 
 ;; first variant
 
-;; TODO: implicit exist for defn-e
+;; TODO: add implicit exist for defn-e
+
+(declare noattack-o)
 
 (defn-e nqueens-o [l]
   ([()])
   ([[[?x ?y] . ?others]]
      (exist []
-      (nqueens-o ?others)
-      (member-o ?y [1 2 3 4 5 6 7 8])
-      (noattack-o [?x ?y] ?others))))
-
-;; NOTE: / for lcons, // for llist ?
+            (nqueens-o ?others)
+            (member-o ?y [1 2 3 4 5 6 7 8])
+            (noattack-o [?x ?y] ?others))))
 
 (defn-e noattack-o [q others]
   ([_ ()])
   ([[?x ?y] [[?x1 ?y1] . ?others]]
      (exist []
-      (!= ?y ?y1)
-      (nonrel/project [?y ?y1 ?x ?x1]
-                      (!= (- ?y1 ?y) (- ?x1 ?x))
-                      (!= (- ?y1 ?y) (- ?x ?x1)))
-      (noattack-o [?x ?y] ?others))))
+            (!= ?y ?y1)
+            (nonrel/project [?y ?y1 ?x ?x1]
+                            (!= (- ?y1 ?y) (- ?x1 ?x))
+                            (!= (- ?y1 ?y) (- ?x ?x1)))
+            (noattack-o [?x ?y] ?others))))
+
+(defn solve-nqueens []
+  (run* [q]
+        (exist [y1 y2 y3 y4 y5 y6 y7 y8]
+               (== q [[1 y1] [2 y2] [3 y3] [4 y4] [5 y5] [6 y6] [7 y7] [8 y8]])
+               (nqueens-o q))))
 
 (comment
-  ;; FIXME
-  (run 1 [q]
-       (exist [y1 y2 y3 y4 y5 y6 y7 y8]
-              (== q [[1 y1] [2 y2] [3 y3] [4 y4] [5 y5] [6 y6] [7 y7] [8 y8]])
-              (nqueens-o q)))
+  (take 1 (solve-nqueens))
+
+  ;; 92 solutions
+  (count (solve-nqueens))
 
   ;; 4s
-  (dotimes [_ 10]
-    (time
-     (dotimes [_ 1e2]
-       (doall
-        (run 1 [q]
-             (exist [y1 y2 y3 y4 y5 y6 y7 y8]
-                    (== q [[1 y1] [2 y2] [3 y3] [4 y4] [5 y5] [6 y6] [7 y7] [8 y8]])
-                    (nqueens-o q)))))))
+  ;; about 10X slower that SWI
+  (binding [*occurs-check* false]
+   (dotimes [_ 5]
+     (time
+      (dotimes [_ 100]
+        (doall
+         (take 1 (solve-nqueens)))))))
   )
 
 ;; Bratko pg 344, finite domain, can we get close to this? there is a LOT more
