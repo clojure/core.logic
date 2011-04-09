e8f1ceb3b90bd03716eb8f6f50ebe86c93be1f86
David Nolen <david.nolen@gmail.com>
2011-04-09 13:26:00 -0400

    * src/logos/arithmetic.clj: add basic arithmetic comparison goals, uses nonrel/project

---
+++ src/logos/arithmetic.clj
@@ -0,0 +1,60 @@
+(ns logos.arithmetic
+  (:refer-clojure :exclude [reify == inc = > < >= <=])
+  (:use logos.minikanren
+        [logos.nonrel :only [project]])
+  (:import [logos.minikanren Substitutions]))
+
+(defmacro = [x y]
+  `(fn [a#]
+     (let [wx# (walk a# ~x)
+           wy# (walk a# ~y)]
+       (if (clojure.core/= wx# wy# )
+         a# nil))))
+
+(defmacro > [x y]
+  `(fn [a#]
+     (let [wx# (walk a# ~x)
+           wy# (walk a# ~y)]
+       (if (clojure.core/> wx# wy# )
+         a# nil))))
+
+(defmacro >= [x y]
+  `(fn [a#]
+     (let [wx# (walk a# ~x)
+           wy# (walk a# ~y)]
+       (if (clojure.core/>= wx# wy# )
+         a# nil))))
+
+(defmacro < [x y]
+  `(fn [a#]
+     (let [wx# (walk a# ~x)
+           wy# (walk a# ~y)]
+       (if (clojure.core/< wx# wy# )
+         a# nil))))
+
+(defmacro <= [x y]
+  `(fn [a#]
+     (let [wx# (walk a# ~x)
+           wy# (walk a# ~y)]
+       (if (clojure.core/<= wx# wy#)
+         a# nil))))
+
+(comment
+  (run 1 [q]
+       (== q 100)
+       (> q 3))
+
+  ;; TODO : remove tediousness w/ macro
+  (defn core-sym [sym]
+    (symbol "clojure.core" (str sym)))
+
+  ;; macro-writing macro
+  (defmacro defarithmetic [op]
+    (let [a (gensym "a")]
+      `(defmacro ~op [x# y#]
+         `(fn [~'a]
+            (let [wx# (walk ~'a ~x#)
+                  wy# (walk ~'a ~y#)]
+              (if (~~(core-sym op) wx# wy#)
+                ~'a nil))))))
+  )
