8e587da6f3a60c5bc38345e79c9f8b477e51141f
David Nolen <david.nolen@gmail.com>
2011-04-28 09:13:39 -0400

    * pom.xml: hello pom.xml, goodbye project.clj, sniff.

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject logos "0.6.1-SNAPSHOT"
-  :description "Logic programming for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0-alpha6"]
-                 [org.clojure.contrib/macro-utils "1.3.0-alpha4"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]
-                     [lein-clojars "0.6.0"]]
-  ;; :jvm-opts ["-agentlib:jdwp=transport=dt_socket,address=8021,server=y,suspend=n"
-  ;;            "-agentpath:/Applications/YourKit_Java_Profiler_8.0.29.app/bin/mac/libyjpagent.jnilib"]
-  )
