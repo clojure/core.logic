51f68f93db739e0e5818a66a05fa2a4859e587b6
David Nolen <david.nolen@gmail.com>
2011-04-28 14:27:35 -0400

    * project.clj: added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject core.logic "0.6.1-SNAPSHOT"
+  :description "A Logic Programming Library for Clojure"
+  :source-path "src/main/clojure"
+  :test-path "test/main/clojure"
+  :dev-dependencies [[swank-clojure "1.3.0"]])
