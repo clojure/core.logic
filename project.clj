879112a06485a0a4ba06f09e6c51726e28f36c6d
David Nolen <david.nolen@gmail.com>
2011-04-29 12:46:22 -0400

    * project.clj: tweak project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A Logic Programming Library for Clojure"
   :source-path "src/main/clojure"
   :test-path "test/main/clojure"
-  :dev-dependencies [[swank-clojure "1.3.0"]])
+  :dev-dependencies [[swank-clojure "1.3.0"]
+                     [lein-clojars "0.6.0"]])
