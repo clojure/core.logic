bd9a3440977ed17648d1e57fec2ba52c95657733
David Nolen <david.nolen@gmail.com>
2011-06-02 00:48:28 -0400

    * src/main/clojure/clojure/core/logic/unify.clj: support for '(?x . ?y) style syntax for rest matching when using binding map and unifier.

--- src/main/clojure/clojure/core/logic/unify.clj
+++ src/main/clojure/clojure/core/logic/unify.clj
@@ -9,28 +9,57 @@
 (defn rem-? [s]
   (symbol (apply str (drop 1 (str s)))))
 
+(defn proc-lvar [lvar-expr store]
+  (let [v (if-let [u (@store lvar-expr)]
+            u
+            (mk/lvar (rem-? lvar-expr)))]
+    (swap! store conj [lvar-expr v])
+    v))
+
+(defn lcons-expr? [expr]
+  (and (seq? expr) (some '#{.} (set expr))))
+
+(declare prep*)
+
 (defn replace-lvar [store]
   (fn [expr]
-    (cond
-     (lvarq-sym? expr)
-     (let [v (if-let [u (@store expr)]
-               u
-               (mk/lvar (rem-? expr)))]
-       (swap! store conj [expr v])
-       v)
-     :else expr)))
+    (if (lvarq-sym? expr)
+      (proc-lvar expr store)
+      (if (lcons-expr? expr)
+        (prep* expr store)
+        expr))))
+
+(defn prep*
+  ([expr store] (prep* expr store false false))
+  ([expr store lcons?] (prep* expr store lcons? false))
+  ([expr store lcons? last?]
+     (let [expr (if (and last? (seq expr))
+                  (first expr)
+                  expr)]
+       (cond
+        (lvarq-sym? expr) (proc-lvar expr store)
+        (seq? expr) (if (or lcons? (lcons-expr? expr))
+                      (let [[f & n] expr
+                            skip (= f '.)
+                            tail (prep* n store lcons? skip)]
+                        (if skip
+                          tail
+                          (mk/lcons (prep* f store) tail)))
+                      (postwalk (replace-lvar store) expr))
+        :else expr))))
 
 (defn prep [expr]
-  (let [lvars (atom {})]
-    (with-meta
-      (postwalk (replace-lvar lvars) expr)
-      {:lvars @lvars})))
+  (let [lvars (atom {})
+        prepped (if (lcons-expr? expr)
+                  (prep* expr lvars true)
+                  (postwalk (replace-lvar lvars) expr))]
+    (with-meta prepped {:lvars @lvars})))
 
 (defn unifier* [u w]
   (first
-   (mk/run* [q]
-            (mk/== u w)
-            (mk/== u q))))
+    (mk/run* [q]
+      (mk/== u w)
+      (mk/== u q))))
 
 (defn binding-map* [u w]
   (let [lvars (merge (-> u meta :lvars)
@@ -38,15 +67,19 @@
         s (mk/unify mk/empty-s u w)]
     (when s
       (into {} (map (fn [[k v]]
-                      [k (mk/walk s v)])
+                      [k (mk/reify s v)])
                     lvars)))))
 
 (defn unifier [u w]
+  {:pre [(not (mk/lcons? u))
+         (not (mk/lcons? w))]}
   (let [up (prep u)
         wp (prep w)]
     (unifier* up wp)))
 
 (defn binding-map [u w]
+  {:pre [(not (mk/lcons? u))
+         (not (mk/lcons? w))]}
   (let [up (prep u)
         wp (prep w)]
     (binding-map* up wp)))
