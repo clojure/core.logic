aad003175ccc45284213a4cc312fe001609b4932
David Nolen <david.nolen@gmail.com>
2011-06-12 15:29:16 -0400

    * src/main/clojure/clojure/core/logic/prelude.clj: remove old fact code

--- src/main/clojure/clojure/core/logic/prelude.clj
+++ src/main/clojure/clojure/core/logic/prelude.clj
@@ -31,56 +31,12 @@
   (match/handle-clauses 'clojure.core.logic.nonrel/condu xs cs))
 
 ;; =============================================================================
-;; Facts
-
-(defn index [tuples]
-  (->> tuples
-       (map (fn [[k :as t]] {k #{t}}))
-       (apply merge-with
-              (fn [a b] (set/union a b)))))
-
-(defmacro defrel [name & args]
-  (let [setsym (symbol (str name "-set"))
-        idxsym (symbol (str name "-indexed"))]
-    `(do
-       (def ~setsym (atom #{}))
-       (def ~idxsym (atom {}))
-       (defmacro ~name [~@args]
-         (defrelg '~setsym '~idxsym ~@args)))))
-
-(defn defrelg [setsym idxsym & args]
-  `(fn [a#]
-     (answers a# (deref ~setsym) (deref ~idxsym) [~@args])))
-
-;; NOTE: put in a dosync?
-
-(defmacro fact [rel & tuple]
-  (let [setsym (symbol (str rel "-set"))
-        idxsym (symbol (str rel "-indexed"))]
-    `(do
-       (swap! ~setsym conj [~@tuple])
-       (reset! ~idxsym (index @~setsym))
-       nil)))
+;; Rel
 
 (defn to-stream [aseq]
   (when (seq aseq)
     (choice (first aseq)
             (fn [] (to-stream (next aseq))))))
-
-(defn answers [a aset indexed [f & r :as t]]
-  (let [v (walk a f)
-        aset (if (lvar? v)
-               aset
-               (indexed v))]
-    (to-stream
-     (->> aset
-          (map (fn [cand]
-                 (when-let [a (unify a t cand)]
-                   a)))
-          (remove nil?)))))
-
-;; -----------------------------------------------------------------------------
-;; Rel
 
 (defmacro def-arity-exc-helper []
   (try
