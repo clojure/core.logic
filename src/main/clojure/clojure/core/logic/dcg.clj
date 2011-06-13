8da873bc15a6792eb93bb25516f71e6a76bc0a45
David Nolen <david.nolen@gmail.com>
2011-06-13 19:45:11 -0400

    * src/main/clojure/clojure/core/logic/dcg.clj: better design that avoid symbol capture

--- src/main/clojure/clojure/core/logic/dcg.clj
+++ src/main/clojure/clojure/core/logic/dcg.clj
@@ -2,84 +2,64 @@
   (:refer-clojure :exclude [reify == inc])
   (:use [clojure.core.logic minikanren prelude]))
 
+(defn lsym [n]
+  (gensym (str "l" n "_")))
+
+;; TODO: don't handle syms specially like this
+
+(defn ->lcons [env [m] i]
+  (let [m (if (symbol? m) `(quote ~m) m)]
+    `(== (lcons ~m ~(env i)) ~(env (dec i)))))
+
+(defn handle-clause [env c i]
+  (if (vector? c)
+    (->lcons c i)
+    (let [c (if (seq? c) c (list c))]
+      (concat c [(env (dec i)) (env i)]))))
 
 (defmacro --> [name & clauses]
   (let [c (count clauses)
-        lsym (fn [n]
-               (symbol (str "l" n)))
-        lsyms (map lsym (range 1 (+ c 2)))
-        clauses (map #(if (seq? %) % (list %)) clauses)
-        clauses (map #(concat %1 [%2 %3]) clauses lsyms (rest lsyms))]
+        r (range 1 (+ c 2))
+        lsyms (into [] (map lsym r))
+        f (println lsyms)
+        clauses (map (partial handle-clause lsyms) clauses r)]
     `(defn ~name [~(first lsyms) ~(last lsyms)]
        (exist [~@(butlast (rest lsyms))]
          ~@clauses))))
 
-(defmacro -->e [name & body]
-  )
+(defn handle-cclauses [fsym osym cclause]
+  (let [c (count cclause)
+        r (range 1 (clojure.core/inc c))
+        lsyms (conj (into [fsym] (map lsym r)) osym)
+        clauses (map (partial handle-clause lsym) cclause)]
+    `(exist [~@lsyms]
+       ~@clauses)))
 
-(defmacro def--> [name args & body])
+(defmacro -->e [name & cclauses]
+  (let [fsym (gensym "l1_")
+        osym (gensym "o")]
+   `(defne ~name [~fsym ~osym]
+      ~@(map (partial handle-cclauses fsym osym) cclauses))))
 
-(defmacro def-->e [name args & body])
+(defmacro def--> [name args & clauses]
+  (let [c (count clauses)
+        r (range 1 (+ c 2))
+        lsyms (map lsym r)
+        clauses (map handle-clause clauses r)]
+   `(defn ~name [~@args ~(first lsyms) ~(last lsyms)]
+      (exist [~@(butlast (rest lsyms))]
+        ~@clauses))))
+
+(defmacro def-->e [name args & clauses])
 
 (comment
   (--> s np vp)
   (--> s (np subject) vp)
-  
-  (--> n '[go] (label x) '[to])
-  
-  (-->e n '[witch] ...)
-
+  (--> n [go] [to])
+  (-->e det
+    ([the])
+    ([a]))
   (-->e np-subject
-        ((det) (n))
-        ((pro-subject)))
-
-  (def--> foo [x y]
-    )
-
-  (defn n [l1 l2]
-    (matche l1
-      ()))
-
-  ;; difference lists
-  (run* [q]
-    (exist [x]
-      (== q (llist 1 2 3 x))
-      (== x '(4 5 6))))
-
-  ;; 864ms for 12
-  ;; 980ms for 13
-  ;; 1159ms for 14
-  ;; cost w/o reification?
-  ;; 597 ms w/o reification for 14
-  (dotimes [_ 10]
-    (let [x (lvar 'x)
-          l (llist 1 2 3 4 5 6 7 8 9 x)
-          y (lvar 'y)
-          z (lvar 'z)
-          a (lvar 'a)
-          b (lvar 'b)]
-     (time
-      (dotimes [_ 1e5]
-        (run* [q]
-          (== q l)
-          (== x (lcons 10 y))
-          (== y (lcons 11 z))
-          (== z (lcons 12 a))
-          (== a (lcons 13 b))
-          (== b '(14)))))))
-
-  ;; 140ms
-  (dotimes [_ 10]
-    (time
-     (dotimes [_ 1e5]
-       (doall (concat '(1 2 3 4 6 7 8 9) '(0))))))
-
-  ;; 400ms for 12
-  ;; 560 for 13
-  ;; 744 for 14
-  (dotimes [_ 10]
-    (let [l '(1 2 3 4 6 7 8 9)]
-     (time
-      (dotimes [_ 1e5]
-        (doall (concat (concat (concat (concat (concat l '(10)) '(11)) '(12)) '(13)) '(14)))))))
+    ((det) (n))
+    ((pro-subject)))
   )
