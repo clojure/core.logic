1415a313172b84c640cf6f60f5db2ee2a01242ea
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-03-24 19:01:38 -0400

    * src/test/cljs/cljs/core/logic/tests.cljs: more unification tests

--- src/test/cljs/cljs/core/logic/tests.cljs
+++ src/test/cljs/cljs/core/logic/tests.cljs
@@ -8,6 +8,12 @@
 
 (set! *print-fn* js/print)
 
+;; =============================================================================
+;; unify
+
+;; -----------------------------------------------------------------------------
+;; unify with nil
+
 (let [x (lvar 'x)]
   (assert (= (pair x nil) (pair x nil))))
 
@@ -29,6 +35,9 @@
 
 (let [x (lvar 'x)]
    (assert (= (-unify empty-s nil #{}) false)))
+
+;; -----------------------------------------------------------------------------
+;; unify with object
 
 (assert (= (-unify empty-s 1 nil) false))
 (assert (= (-unify empty-s 1 1) empty-s))
@@ -52,3 +61,53 @@
 (assert (= (-unify empty-s 1 '[]) false))
 (assert (= (-unify empty-s 1 {}) false))
 (assert (= (-unify empty-s 1 #{}) false))
+
+;; -----------------------------------------------------------------------------
+;; unify with lvar
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x 1)]
+  (assert (= (-unify empty-s x 1) os)))
+
+(let [x (lvar 'x)
+      y (lvar 'y)
+      os (-ext-no-check empty-s x y)]
+  (assert (= (-unify empty-s x y) os)))
+
+(let [x (lvar 'x)
+      y (lvar 'y)
+      l (lcons 1 y)
+      os (-ext-no-check empty-s x l)]
+  (assert (= (-unify empty-s x l) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x [])]
+  (assert (= (-unify empty-s x []) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x [1 2 3])]
+  (assert (= (-unify empty-s x [1 2 3]) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x '())]
+  (assert (= (-unify empty-s x '()) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x '(1 2 3))]
+  (assert (= (-unify empty-s x '(1 2 3)) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x {})]
+  (assert (= (-unify empty-s x {}) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x {1 2 3 4})]
+  (assert (= (-unify empty-s x {1 2 3 4}) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x #{})]
+  (assert (= (-unify empty-s x #{}) os)))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x #{1 2 3})]
+  (assert (= (-unify empty-s x #{1 2 3}) os)))
