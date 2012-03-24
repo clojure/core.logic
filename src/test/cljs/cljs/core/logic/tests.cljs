366a4d5685237bd7604cb00fca191d3297b666fe
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-03-24 18:57:48 -0400

    * src/test/cljs/cljs/core/logic/tests.cljs: porting over CLJ core.logic tests

--- src/test/cljs/cljs/core/logic/tests.cljs
+++ src/test/cljs/cljs/core/logic/tests.cljs
@@ -4,13 +4,22 @@
     :only [run run* == conde fresh defne matche]])
   (:use
    [cljs.core.logic
-    :only [lvar lcons -unify -ext-no-check empty-s]]))
+    :only [pair lvar lcons -unify -ext-no-check empty-s]]))
+
+(set! *print-fn* js/print)
+
+(let [x (lvar 'x)]
+  (assert (= (pair x nil) (pair x nil))))
+
+(let [x (lvar 'x)]
+  (assert (false? (= (pair x nil) (pair nil x)))))
 
 (assert (= (-unify empty-s nil 1) false))
 
 (let [x (lvar 'x)
-      os (-ext-no-check empty-s x nil)]
-  (assert (= (-unify empty-s nil x) os)))
+      a (-ext-no-check empty-s x nil)
+      b (-unify empty-s nil x)]
+  (assert (= a b)))
 
 (let [x (lvar 'x)]
   (assert (= (-unify empty-s nil (lcons 1 x)) false)))
@@ -19,4 +28,27 @@
   (assert (= (-unify empty-s nil {}) false)))
 
 (let [x (lvar 'x)]
-  (assert (= (-unify empty-s nil #{}) false)))
+   (assert (= (-unify empty-s nil #{}) false)))
+
+(assert (= (-unify empty-s 1 nil) false))
+(assert (= (-unify empty-s 1 1) empty-s))
+(assert (= (-unify empty-s :foo :foo) empty-s))
+(assert (= (-unify empty-s 'foo 'foo) empty-s))
+(assert (= (-unify empty-s "foo" "foo") empty-s))
+(assert (= (-unify empty-s 1 2) false))
+(assert (= (-unify empty-s 2 1) false))
+(assert (= (-unify empty-s :foo :bar) false))
+(assert (= (-unify empty-s 'foo 'bar) false))
+(assert (= (-unify empty-s "foo" "bar") false))
+
+(let [x (lvar 'x)
+      os (-ext-no-check empty-s x 1)]
+  (assert (= (-unify empty-s 1 x) os)))
+
+(let [x (lvar 'x)]
+  (assert (= (-unify empty-s 1 (lcons 1 'x)) false)))
+
+(assert (= (-unify empty-s 1 '()) false))
+(assert (= (-unify empty-s 1 '[]) false))
+(assert (= (-unify empty-s 1 {}) false))
+(assert (= (-unify empty-s 1 #{}) false))
