f45cc9d41a26fe6924cb989d99ea771f4d37f68f
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-07-13 10:42:43 -0400

    * src/main/clojure/clojure/core/logic/bench.clj: another tweak

--- src/main/clojure/clojure/core/logic/bench.clj
+++ src/main/clojure/clojure/core/logic/bench.clj
@@ -332,14 +332,14 @@
       (distinctfd [a b c d])
       (== a 1)
       (<=fd a b) (<=fd b c) (<=fd c d)
-      (+fd a b s1) #_(+fd s1 c s2) #_(+fd s2 d n)
+      (+fd a b s1) (+fd s1 c s2) (+fd s2 d n)
       (checko [a b c d] () () n)
       (== q [a b c d]))))
 
 (comment
   (matches 40)
   
-  ;; 20ms
+  ;; 20ms, ~130ms if we add the other sums
   (dotimes [_ 5]
     (time
      (dotimes [_ 1]
