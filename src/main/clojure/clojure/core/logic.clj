f0e14f3035181376af2378b051534dbaa35b637a
David Nolen <david.nolen@nytimes.com>
2012-09-29 17:37:56 -0400

    note that force-ans is really Mozart/OZ's "distribute" step

--- src/main/clojure/clojure/core/logic.clj
+++ src/main/clojure/clojure/core/logic.clj
@@ -232,6 +232,8 @@
 ;; -----------------------------------------------------------------------------
 ;; force-ans support
 
+;; TODO: this is really Mozart/OZ "distribute"
+
 (defprotocol IForceAnswerTerm
   (-force-ans [v x]))
 
