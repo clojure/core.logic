d11ce301f60a1bf375a3dd8413584e6f54e73410
David Nolen <dnolen@Davids-MacBook-Pro-2.local>
2012-12-07 08:25:36 +0000

    update project.clj

--- project.clj
+++ project.clj
@@ -1,21 +1,30 @@
 (defproject org.clojure/core.logic "0.8.0-beta3-SNAPSHOT" 
   :description "A logic/relational programming library for Clojure"
-  :extra-classpath-dirs ["checkouts/clojurescript/src/clj"
-                         "checkouts/clojurescript/src/cljs"]
   :parent [org.clojure/pom.contrib "0.0.25"]
 
   ;; lein 1
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
+  :dev-dependencies [[lein-cljsbuild "0.2.9"]]
+  ; :extra-classpath-dirs ["checkouts/clojurescript/src/clj"
+  ;                        "checkouts/clojurescript/src/cljs"]
 
   ;; lein 2
-  :source-paths ["src/main/clojure"]
+  :source-paths ["src/main/clojure"
+                 ;"clojurescript/src/clj"
+                 ;"clojurescript/src/cljs"
+                 ]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojurescript "0.0-1535"]
+                 [org.clojure/google-closure-library "0.0-2029"]
+                 [org.clojure/google-closure-library-third-party "0.0-2029"] 
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.nrepl "0.2.0-RC1"]
                  [com.datomic/datomic-free "0.8.3551" :scope "provided"]]
-  :dev-dependencies [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.9"]]
+
+
   :cljsbuild {:builds {:test-simp {:source-path "src/test/cljs"
                                    :compiler {:optimizations :simple
                                               :pretty-print true
