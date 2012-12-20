a19f0b425c1ac46f1d1811974ffde00bc27f3677
Nada Amin <namin@alum.mit.edu>
2012-12-20 02:21:18 +0100

    formatting, take 2

--- src/main/clojure/clojure/core/logic.clj
+++ src/main/clojure/clojure/core/logic.clj
@@ -2921,9 +2921,9 @@
           a  (reduce (fn [a c]
                        (queue a c))
                (assoc a :cq (or cq [])) xcs)]
-       (if cq
-         a
-         (fix-constraints a)))))
+     (if cq
+       a
+       (fix-constraints a)))))
 
 (defn run-constraints* [xs cs ws]
   (if (or (zero? (count cs))
