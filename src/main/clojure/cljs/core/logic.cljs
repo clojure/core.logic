115a9925cebe4a4b49ce607860ce647ec1d2e10b
Alex Miller <alex.miller@cognitect.com>
2021-11-04 21:26:50 -0500

    fix docstring for appendo

--- src/main/clojure/cljs/core/logic.cljs
+++ src/main/clojure/cljs/core/logic.cljs
@@ -750,7 +750,7 @@
 
 (defne appendo 
   "A relation where x, y, and z are proper collections, 
-  such that z is x appended to y"
+  such that z is y appended to x"
   [x y z]
   ([() _ y])
   ([[a . d] _ [a . r]] (appendo d y r)))
